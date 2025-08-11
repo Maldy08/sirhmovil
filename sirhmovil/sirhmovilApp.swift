97de05da8c0c270b7ce2216a39d15daa0b201722
Carlos Maldonado <32904610+Maldy08@users.noreply.github.com>
2025-08-10 20:18:48 -0700

    Initial Commit

---
+++ sirhmovil/sirhmovilApp.swift
@@ -0,0 +1,17 @@
+//
+//  sirhmovilApp.swift
+//  sirhmovil
+//
+//  Created by Carlos Maldonado on 10/08/25.
+//
+
+import SwiftUI
+
+@main
+struct sirhmovilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
