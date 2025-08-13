7a985243a73f718e512af30ff6ac966c07e6d2eb
Carlos Maldonado <32904610+Maldy08@users.noreply.github.com>
2025-08-12 17:25:22 -0700

    initial commit

---
+++ sirhmovil/app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sirhmovil
+//
+//  Created by Carlos Maldonado on 10/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
